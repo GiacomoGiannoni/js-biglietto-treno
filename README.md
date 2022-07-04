3b49178f5d41a21af507149d660d6210111da7c5
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-04 16:53:51 +0200

    fine parte età e sconto

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # js-biglietto-treno
+
+
+- chiedo i km che deve percorrere
+- chiedo l'eta della persona
+- SE l'eta è minore di 65, applico il 40% di sconto
+- Se l'eta è superiore a 18, applico il 20% di sconto
+- se l'eta è tra 18 e 65, nessuno sconto disponibile
---
+++ js/main.js
@@ -0,0 +1,21 @@
+const kmdaPercorrere = prompt('Inserisci Km da percorrere');
+console.log("Il tipo della variabile kmdaPercorrere è: " + typeof kmdaPercorrere)
+const annoNascita = prompt('Inserisci il tuo anno di nascita');
+console.log("Il tipo della variabile annoNascita è: " + typeof annoNascita);
+
+const annoNascitaNumerico = parseInt(annoNascita);
+console.log("Il tipo della variabile annoNascitaNumerico è: " +typeof annoNascitaNumerico);
+
+const adesso = new Date('2022');
+
+const annoCorrente = adesso.getFullYear('2022');
+const eta = annoCorrente - annoNascitaNumerico;
+alert('La tua età è: ' + eta + ' anni');
+if (eta > 65) {
+    alert('Hai uno sconto del 40%')
+} else if (eta < 18) {
+    alert('Hai uno sconto del 20%')
+} else {
+    alert('Nessuno sconto disponibile')
+}
+
